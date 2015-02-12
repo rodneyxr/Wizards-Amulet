dd61c6fb4cc7315028e7b3edb902e1a1fdf3b1d2
Sean <seanmgallagher12@gmail.com>
2015-02-12 17:21:14 -0600

    Merge branch 'master' of https://bitbucket.org/cbarber1592/gamedev-proj into SeanBranch
    Conflicts:
    	Assets/Resources/Scripts/Player/PlayerMove.cs
    	Assets/Scenes/Level01.unity

---
+++ Assets/Resources/Scripts/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+	public GameObject thing;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Interact(){
+		Portcullis controller = thing.GetComponent<Portcullis> ();
+		if (controller.open)
+			controller.open = false;
+		else
+			controller.open = true;
+	}
+}
