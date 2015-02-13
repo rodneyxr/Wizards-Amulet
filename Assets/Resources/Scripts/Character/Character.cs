37cbcf7652fbb361c3e9ab279d0462f6c4217b52
rodneyxr <rodneyxr@gmail.com>
2015-02-12 19:46:53 -0600

    updated gitignore and bug fixes

--- Assets/Resources/Scripts/Character/Character.cs
+++ Assets/Resources/Scripts/Character/Character.cs
@@ -7,50 +7,50 @@ using System.Collections;
 */
 public abstract class Character : MonoBehaviour {
 
-	//Character Health and Mana
-	private int health;
-	private int mana;
+    //Character Health and Mana
+    private int health;
+    private int mana;
 
-	//Character Skills
-	private int charClass;
+    //Character Skills
+    private int charClass;
 
-	//Character Specific Stats
-	private float moveSpeed;
-	private int attackDamage;
-	
-	//functions
-	public int Health{
-		get{return health;}
-		set{health = value;}
-	}
-	public int Mana{
-		get{return mana;}
-		set{mana = value;}
-	}
-	public int CharClass{
-		get{return charClass;}
-		set{charClass = value;}
-	}
-	public float MoveSpeed{
-		get{return MoveSpeed;}
-		set{MoveSpeed = value;}
-	}
-	public int AttackDamage{
-		get{return attackDamage;}
-		set{attackDamage = value;}
-	}
+    //Character Specific Stats
+    private float moveSpeed;
+    private int attackDamage;
 
-	public abstract void increaseHealth(int healthIncreaseAmount);
-	public abstract void decreaseHealth(int healthDecreaseAmount);
-	public abstract void decreaseMana (int manaDecreaseAmount);
-	public abstract void increaseMana (int manaIncreaseAmount);
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //functions
+    public int Health {
+        get { return health; }
+        set { health = value; }
+    }
+    public int Mana {
+        get { return mana; }
+        set { mana = value; }
+    }
+    public int CharClass {
+        get { return charClass; }
+        set { charClass = value; }
+    }
+    public float MoveSpeed {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+    public int AttackDamage {
+        get { return attackDamage; }
+        set { attackDamage = value; }
+    }
+
+    public abstract void increaseHealth(int healthIncreaseAmount);
+    public abstract void decreaseHealth(int healthDecreaseAmount);
+    public abstract void decreaseMana(int manaDecreaseAmount);
+    public abstract void increaseMana(int manaIncreaseAmount);
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
