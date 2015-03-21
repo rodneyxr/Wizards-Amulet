a3101c3158e0c10de2cdae5b36c2797718b9da6c
Sean <seanmgallagher12@gmail.com>
2015-03-21 16:24:58 -0500

    no message

---
+++ Assets/Resources/Scripts/ChangeLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+	public string direction;
+	public string level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		print ("test");
+		if(other.tag.Equals("Player")){
+			StartLevel s = GameObject.Find("_Main").GetComponent<StartLevel>();
+			s.StartNewLevel(level, direction);
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/StartLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartNewLevel(string level, string direction){
+		string tile = "StartTile";
+		if(direction.Equals ("EndTile"))
+		   tile = direction;
+
+		GameObject player = GameObject.Find ("Player");
+		DontDestroyOnLoad(player);
+		DontDestroyOnLoad (gameObject);
+		Application.LoadLevel (level);
+		GameObject startTile = GameObject.Find (tile);
+		player.transform.position = new Vector3 (startTile.transform.position.x,
+		                                        player.transform.position.y,
+		                                        startTile.transform.position.z);
+
+	}
+}
