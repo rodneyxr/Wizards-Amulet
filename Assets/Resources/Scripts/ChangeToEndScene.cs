fd38211966e42ee13fc5dc2bf811c9856469d03d
matteiser <matteisermann@gmail.com>
2015-04-23 13:20:15 -0500

    Matts changes

--- Assets/Resources/Scripts/ChangeToEndScene.cs
+++ Assets/Resources/Scripts/ChangeToEndScene.cs
@@ -8,6 +8,11 @@ public class ChangeToEndScene : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider other) {
 		if (other.tag.Equals("Player")) {
+			GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+			for (int i = 0; i < GameObjects.Length; i++)
+			{
+				Destroy(GameObjects[i]);
+			}
 			Application.LoadLevel(level);
 		}
 	}
